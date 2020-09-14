65e7eb354adddc1f62a39a4225d5f2cd9a4c4525
Stephen Marshall <stephenmarshall@outlook.com>
2020-09-14 21:19:20 +0100

    Use Markdown and GraphQL page query

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,19 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Portfolio`,
+    description: `Personal website for Stephen Marshall, web developer based in London.`,
+    author: `Stephen Marshall`,
   },
   plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/src/content`,
+      },
+    },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
---
+++ src/content/hero.md
@@ -0,0 +1,9 @@
+---
+greetings: "Hi"
+emoji: "👋"
+title: "I'm Stephen Marshall"
+subtitlePrefix: "I build and design "
+subtitleHighlight: "things for the web."
+---
+
+Based in London.
